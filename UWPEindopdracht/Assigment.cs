7455f0816c5033079a66ad8541e3a21542c9f0f9
Bart Machielsen <bartmachielsen@gmail.com>
2016-12-09 19:08:16 +0100

    changed interface to abstract class for less code and better structure

--- UWPEindopdracht/Assigment.cs
+++ UWPEindopdracht/Assigment.cs
@@ -13,7 +13,34 @@ namespace UWPEindopdracht
 
     public class AssigmentHelper
     {
-        public static async Task<Place> pickTargetPlace(Place[] places, GCoordinate currentPosition, int minDistance, int maxDistance)
+       
+
+    }
+    abstract class Assigment
+    {
+        public int maxDistance { get; set; }
+        public int minDistance { get; set; }
+        public int? maxSpeed { get; set; }
+        public string description { get; set; }
+        public Place target { get; set; }
+        public TimeSpan maximumTime { get; set; }
+
+
+        public async void fillTarget(Place[] places, GCoordinate currentPosition)
+        {
+            target = await pickTargetPlace(places, currentPosition);
+            maximumTime = (await GPSHelper.calculateRouteBetween(currentPosition, target.Location)).EstimatedDuration;
+            fillDescription();
+        }
+
+        public abstract void fillDescription();
+        /// <summary>
+        /// Select target random from a list with only items that are on a good distance from source
+        /// </summary>
+        /// <param name="places"></param>
+        /// <param name="currentPosition"></param>
+        /// <returns></returns>
+        public async Task<Place> pickTargetPlace(Place[] places, GCoordinate currentPosition)
         {
             List<Place> sortedPlaces = new List<Place>(places);
             foreach (var place in sortedPlaces)
@@ -27,7 +54,13 @@ namespace UWPEindopdracht
             Random random = new Random();
             return sortedPlaces.ElementAt(random.Next(sortedPlaces.Count));
         }
-        public static  double totalScore(TimeSpan currentTimeSpent, Place target, TimeSpan maximumTime)
+
+        /// <summary>
+        /// Calculating the total reachable score with the current time
+        /// </summary>
+        /// <param name="currentTimeSpent"></param>
+        /// <returns></returns>
+        public double totalScore(TimeSpan currentTimeSpent)
         {
             double distance = 0;
             double timebonus = 0;
@@ -38,71 +71,46 @@ namespace UWPEindopdracht
 
             return distance * 10 + timebonus * 15;
         }
-
-    }
-    interface IAssigment
-    {
-        int maxDistance { get; set; }
-        int minDistance { get; set; }
-        int? maxSpeed { get; set; }
-        string description { get; set; }
-        Place target { get; set; }
-        TimeSpan maximumTime { get; set; }
-        
-        void fillTarget(Place[] places, GCoordinate currentPosition);
-
-        // TODO ADD VIEW FOR SHOWING ASSIGMEMT INFORMATION
     }
 
-    class MapAssigment : IAssigment
+    class MapAssigment : Assigment
     {
-        public int maxDistance { get; set; } = 5000;
-        public int minDistance { get; set; } = 1000;
-        public int? maxSpeed { get; set; } = 30;
-        public string description { get; set; }
-        public Place target { get; set; }
-        public TimeSpan maximumTime { get; set; }
-
-
-        
-
-
-        public async void fillTarget(Place[] places, GCoordinate currentPosition)
+        public MapAssigment()
         {
-            target = await AssigmentHelper.pickTargetPlace(places, currentPosition, minDistance, maxDistance);
-            maximumTime = (await GPSHelper.calculateRouteBetween(currentPosition, target.Location)).EstimatedDuration;
+            maxDistance = 5000;
+            minDistance = 1000;
+            maxSpeed = 40;
+        }
+
+        public override void fillDescription()
+        {
             description =
                 "Walk to the marked point on the map, " +
                 $"\n Bonus if reached within {maximumTime.TotalMinutes} minutes." +
-                $"\n total score could be {AssigmentHelper.totalScore(new TimeSpan(), target, maximumTime)}!";
+                $"\n total score could be {totalScore(new TimeSpan())}!";
         }
-
-
-       
     }
 
 
-    class SearchAssigment : IAssigment
+    class SearchAssigment : Assigment
     {
-        public int maxDistance { get; set; } = 1000;
-        public int minDistance { get; set; } = 100;
-        public int? maxSpeed { get; set; } = 100;
-        public string description { get; set; }
-        public Place target { get; set; }
-        public TimeSpan maximumTime { get; set; } = new TimeSpan(0,1,0,0);
-    
-
-        public async void fillTarget(Place[] places, GCoordinate currentPosition)
+        public SearchAssigment()
         {
-            target = await AssigmentHelper.pickTargetPlace(places, currentPosition, minDistance, maxDistance);
-            maximumTime = (await GPSHelper.calculateRouteBetween(currentPosition, target.Location)).EstimatedDuration;
+            maximumTime = new TimeSpan(0,1,0,0);
+            maxSpeed = 1000;
+            maxDistance = 1000;
+            minDistance = 100;
+        }
+
+
+        public override void fillDescription()
+        {
             description =
                 "Search the given point!" +
                 $"\n Name: {target.Name}" +
                 $"\n Estimated distance to the target {target.Distance}!" +
                 $"\n Bonus if reached within 60 minutes!" +
-                $"\n Maximum score is {AssigmentHelper.totalScore(new TimeSpan(), target, maximumTime)}";
+                $"\n Maximum score is {totalScore(new TimeSpan())}";
         }
-
     }
 }
